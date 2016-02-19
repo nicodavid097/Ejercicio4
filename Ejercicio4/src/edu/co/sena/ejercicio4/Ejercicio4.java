ee67fbcd1940cdd0b54faa53fd0811cc7e9afb62
Nicolas <ndhernandez2@misena.edu.co>
2016-02-18 21:48:49 -0500

    commir ejercicio 5

---
+++ Ejercicio4/src/edu/co/sena/ejercicio4/Ejercicio4.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.co.sena.ejercicio4;
+
+/**
+ *
+ * @author NICO-DAVID
+ */
+public class Ejercicio4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+
+        int a, b;
+        a = 5;
+        b = 10;
+        System.out.println("a+b = " + (a + b));
+
+        System.out.println("a-b = " + (a - b));
+
+        System.out.println("a/b = " + (a / b));
+
+        System.out.println("a*b = " + (a * b));
+    }
+
+}
